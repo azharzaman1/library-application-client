268b553131c1bce7c2421fd75f2d73dda1a661f8
Azhar Zaman <68331974+azharzaman1@users.noreply.github.com>
2022-03-14 12:01:37 +0500

    Starting Up

---
+++ src/components/Generic/Header/index.js
@@ -0,0 +1,12 @@
+import { FormControlLabel, FormGroup, Switch } from "@mui/material";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between">
+      <div>Library</div>
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Header;
