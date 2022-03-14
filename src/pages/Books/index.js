ea49cc155ef0460494c4efaba3bdd135ff33b2eb
Azhar Zaman <68331974+azharzaman1@users.noreply.github.com>
2022-03-14 14:41:53 +0500

    Popups implemented

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -1,11 +1,22 @@
 import { Add } from "@mui/icons-material";
-import { AppBar, Box, Button, Toolbar } from "@mui/material";
-import React from "react";
-import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  TextField,
+  Toolbar,
+} from "@mui/material";
+import React, { useState } from "react";
+import Dialog from "../../components/Generic/Dialog";
 import Heading from "../../components/Generic/Heading";
 import Container from "../../components/Generic/Layout/Container";
 
 import StudentsTable from "../../components/Generic/Table";
+import Text from "../../components/Generic/Text";
 
 const columns = [
   { field: "id", headerName: "ID", width: 90 },
@@ -41,14 +52,6 @@ const columns = [
   },
 ];
 
-/*
-{
-    borrowedBy: { type: String },
-    borrowedOn: { type: Date },
-    returnDate: { type: Date },
-  },
-*/
-
 const books = [
   { class: "MB-1", id: 1, lastName: "Snow", firstName: "Jon" },
   { class: "MB-1", id: 2, lastName: "Lannister", firstName: "Cersei" },
@@ -68,6 +71,13 @@ const books = [
 ];
 
 const Books = () => {
+  const [addNewDialogOpen, setAddNewDialogOpen] = useState(false);
+  const [available, setAvailable] = useState(false);
+
+  const handleAvailableChange = (e) => {
+    setAvailable(e.target.checked);
+  };
+
   return (
     <div>
       <Container maxWidth={false}>
@@ -82,7 +92,12 @@ const Books = () => {
                 Books
               </Heading>
               <div className="appBar__right flex-1 flex justify-end">
-                <Button color="primary" variant="contained" endIcon={<Add />}>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  endIcon={<Add />}
+                  onClick={() => setAddNewDialogOpen(true)}
+                >
                   Add Book
                 </Button>
               </div>
@@ -93,6 +108,91 @@ const Books = () => {
           <StudentsTable columns={columns} tableData={books} />
         </main>
       </Container>
+
+      <Dialog
+        dialogTitle="Add New Book"
+        open={addNewDialogOpen}
+        setOpen={setAddNewDialogOpen}
+        confirmAction={() => console.log("Confirmed")}
+        confirmActionLabel="Add Book"
+        discardActionLabel="Discard"
+      >
+        {/* Dialog Content */}
+        <div>
+          <div className="pb-5">
+            <Text>Fill in info and hit Add New</Text>
+          </div>
+          <Grid container columnSpacing={2} rowSpacing={2}>
+            <Grid item sx={12}>
+              <TextField
+                id="book-name"
+                label="Book Name"
+                fullWidth
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item sx={12}>
+              <TextField
+                id="author-name"
+                label="Author Name"
+                fullWidth
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item sx={12}>
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={available}
+                      onChange={handleAvailableChange}
+                    />
+                  }
+                  label="Available for borrow"
+                />
+              </FormGroup>
+            </Grid>
+            {!available && (
+              <Grid item container columnSpacing={2} rowSpacing={3}>
+                <Grid item sx={12}>
+                  <TextField
+                    id="borrowed-by"
+                    label="Borrowed By"
+                    fullWidth
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item sx={12}>
+                  <TextField
+                    fullWidth
+                    id="borrow-date"
+                    label="Borrowed On"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    sx={{ width: 220 }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </Grid>
+                <Grid item sx={12}>
+                  <TextField
+                    fullWidth
+                    id="return-date"
+                    label="Expected return date"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    sx={{ width: 220 }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </Grid>
+              </Grid>
+            )}
+          </Grid>
+        </div>
+      </Dialog>
     </div>
   );
 };
