e9566ef08809ce6e53e898d3f2ec98347a40a1b0
Azhar Zaman <68331974+azharzaman1@users.noreply.github.com>
2022-03-21 12:16:45 +0500

    Chips Added

--- src/pages/DesignKit.js
+++ src/pages/DesignKit.js
@@ -1,6 +1,7 @@
 import { Google, Save, Send } from "@mui/icons-material";
-import { Divider } from "@mui/material";
+import { Divider, Grid, Stack } from "@mui/material";
 import Button from "../components/Generic/Button";
+import Chip from "../components/Generic/Chip";
 import Heading from "../components/Generic/Heading";
 import Container from "../components/Generic/Layout/Container";
 
@@ -8,7 +9,7 @@ const DesignKit = () => {
   return (
     <div>
       <Container maxWidth="md" className="min-h-screen">
-        <div className="mt-3 px-3 py-3 shadow-lg bg-white">
+        <div className="my-3 px-3 py-3 shadow-lg bg-white">
           <div className="header py-2 px-2 flex items-center justify-between rounded-md bg-secondary">
             <Heading type="secondary">DesignKit</Heading>
             <div>Switch</div>
@@ -26,21 +27,40 @@ const DesignKit = () => {
                       <Divider />
                     </div>
 
-                    <div className="py-5 flex items-center justify-between flex-wrap">
-                      <Button>Primary Button</Button>
-                      <Button type="secondary">Secondary Button</Button>
-
-                      <Button type="icon" startIcon={<Save />}>
-                        Icon Button
-                      </Button>
-                      <Button type="icon" endIcon={<Send />}>
-                        Icon Button
-                      </Button>
-                      <Button type="special-icon">
-                        <Google />
-                      </Button>
-                      <Button type="text">Text Button</Button>
-                    </div>
+                    <Grid
+                      container
+                      direction="row"
+                      alignItems="center"
+                      justifyContent="space-evenly"
+                      rowSpacing={2}
+                      columnSpacing={2}
+                      className="py-5"
+                    >
+                      <Grid item>
+                        <Button>Primary Button</Button>
+                      </Grid>
+                      <Grid item>
+                        <Button type="secondary">Secondary Button</Button>
+                      </Grid>
+                      <Grid item>
+                        <Button type="icon" startIcon={<Save />}>
+                          Icon Button
+                        </Button>
+                      </Grid>
+                      <Grid item>
+                        <Button type="icon" endIcon={<Send />}>
+                          Icon Button
+                        </Button>
+                      </Grid>
+                      <Grid item>
+                        <Button type="special-icon">
+                          <Google />
+                        </Button>
+                      </Grid>
+                      <Grid item>
+                        <Button type="text">Text Button</Button>
+                      </Grid>
+                    </Grid>
                   </div>
                   <div>
                     <div className="mt-2">
@@ -48,28 +68,141 @@ const DesignKit = () => {
                       <Divider />
                     </div>
 
-                    <div className="py-5 flex items-center space-x-3 flex-wrap">
-                      <Button loading>Primary Button</Button>
-                      <Button
-                        type="icon"
-                        loading
-                        startIcon={<Save fontSize="small" />}
+                    <Grid
+                      container
+                      direction="row"
+                      alignItems="center"
+                      justifyContent="space-evenly"
+                      rowSpacing={2}
+                      columnSpacing={2}
+                      className="py-5"
+                    >
+                      <Grid item>
+                        <Button loading>Primary Button</Button>
+                      </Grid>
+                      <Grid item>
+                        <Button
+                          type="icon"
+                          loading
+                          startIcon={<Save fontSize="small" />}
+                        >
+                          Icon Button
+                        </Button>
+                      </Grid>
+                      <Grid item>
+                        <Button
+                          type="icon"
+                          loading
+                          endIcon={<Send fontSize="small" />}
+                        >
+                          Icon Button
+                        </Button>
+                      </Grid>
+                      <Grid item>
+                        <Button type="special-icon" loading>
+                          <Google />
+                        </Button>
+                      </Grid>
+                      <Grid item>
+                        <Button type="text" loading>
+                          Text Button
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="kit-section">
+              <Heading type="secondary">Chips</Heading>
+              <div className="mt-2">
+                {/* Chips */}
+                <div className="rounded-md border px-3">
+                  {/* Normal Chips */}
+                  <div>
+                    <div>
+                      <div className="mt-2">
+                        <Heading type="tertiary">Normal</Heading>
+                        <Divider />
+                      </div>
+
+                      <Grid
+                        container
+                        direction="row"
+                        alignItems="center"
+                        justifyContent="space-evenly"
+                        rowSpacing={2}
+                        columnSpacing={2}
+                        className="py-5"
                       >
-                        Icon Button
-                      </Button>
-                      <Button
-                        type="icon"
-                        loading
-                        endIcon={<Send fontSize="small" />}
+                        <Grid item>
+                          <Chip color="light">Default Chip Outlined</Chip>
+                        </Grid>
+                        <Grid item>
+                          <Chip color="lightContained">
+                            Light Chip Contained
+                          </Chip>
+                        </Grid>
+                        <Grid item>
+                          <Chip color="primary">Primary Chip Outlined</Chip>
+                        </Grid>
+                        <Grid item>
+                          <Chip color="primaryContained">
+                            Primary Chip Contained
+                          </Chip>
+                        </Grid>
+                      </Grid>
+                    </div>
+                    <div>
+                      <div className="mt-2">
+                        <Heading type="tertiary">Icons</Heading>
+                        <Divider />
+                      </div>
+
+                      <Grid
+                        container
+                        direction="row"
+                        alignItems="center"
+                        justifyContent="space-evenly"
+                        rowSpacing={2}
+                        columnSpacing={2}
+                        className="py-5"
                       >
-                        Icon Button
-                      </Button>
-                      <Button type="special-icon" loading>
-                        <Google />
-                      </Button>
-                      <Button type="text" loading>
-                        Text Button
-                      </Button>
+                        <Grid item>
+                          <Chip
+                            color="light"
+                            startIcon={<Save fontSize="small" />}
+                          >
+                            Default Chip Outlined
+                          </Chip>
+                        </Grid>
+                        <Grid item>
+                          <Chip
+                            color="lightContained"
+                            endIcon={<Send fontSize="small" />}
+                          >
+                            Light Chip Contained
+                          </Chip>
+                        </Grid>
+
+                        <Grid item>
+                          <Chip
+                            color="primary"
+                            startIcon={<Save fontSize="small" />}
+                          >
+                            Primary Chip Outlined
+                          </Chip>
+                        </Grid>
+                        <Grid item>
+                          <Chip
+                            color="primaryContained"
+                            endIcon={<Send fontSize="small" />}
+                          >
+                            Primary Chip Contained
+                          </Chip>
+                        </Grid>
+                      </Grid>
                     </div>
                   </div>
                 </div>
